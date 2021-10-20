5f4b01d4eb35a0dc288e3045a41579a5f04ab51f
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2021-10-20 21:10:09 +0900

    Replace deprecated methods.

--- list_for_profile.rb
+++ list_for_profile.rb
@@ -6,6 +6,6 @@ Plugin.create :list_for_profile do
   user_fragment :list, _("リスト") do
     set_icon Skin.get_path("list.png")
     container = Plugin::ListForProfile::ProfileTab.new(Plugin[:list_for_profile], model)
-    nativewidget ::Gtk::HBox.new.add(container.show_all).closeup(::Gtk::VScrollbar.new(container.vadjustment))
+    nativewidget ::Gtk::Box.new(:horizontal).add(container.show_all).pack_start(::Gtk::VScrollbar.new(container.vadjustment), expand: false)
   end
 end
